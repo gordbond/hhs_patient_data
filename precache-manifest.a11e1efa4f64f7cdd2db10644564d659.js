0bdf4450fec7c8728edcef3f610a911de1b1b525
Gord Bond <gordbondartist@gmail.com>
2020-02-12 13:53:16 -0500

    Updates

---
+++ precache-manifest.a11e1efa4f64f7cdd2db10644564d659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7175ae1a543ab4b9caf2e9a437d51929",
+    "url": "/hhs_patient_data/index.html"
+  },
+  {
+    "revision": "e95c5c9d42027133f1bd",
+    "url": "/hhs_patient_data/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "25fe556fa599640f307f",
+    "url": "/hhs_patient_data/static/css/main.9817bff4.chunk.css"
+  },
+  {
+    "revision": "e95c5c9d42027133f1bd",
+    "url": "/hhs_patient_data/static/js/2.78037cc8.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/hhs_patient_data/static/js/2.78037cc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fe556fa599640f307f",
+    "url": "/hhs_patient_data/static/js/main.dcb010a4.chunk.js"
+  },
+  {
+    "revision": "cd026d8ef4caf92fc7dc",
+    "url": "/hhs_patient_data/static/js/runtime-main.5335d8ba.js"
+  }
+]);
